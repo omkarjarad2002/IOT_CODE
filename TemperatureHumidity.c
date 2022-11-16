870ef19f6f00db523cb72a8166935eb7cf2f0c70
omkar <omkarjarad2019@gmail.com>
2022-11-16 09:11:26 +0530

    3

--- TemperatureHumidity.c
+++ TemperatureHumidity.c
@@ -1,3 +1,37 @@
+// Servo Motor
+#include <Servo.h>
+int V_distance = 0;
+Servo servo_6;
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+    pinMode(triggerPin, OUTPUT); // Clear the trigger
+    digitalWrite(triggerPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigger pin to HIGH state for 10 microseconds
+    digitalWrite(triggerPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(triggerPin, LOW);
+    pinMode(echoPin, INPUT);
+    // Reads the echo pin, and returns the sound wave travel time in microseconds
+    return pulseIn(echoPin, HIGH);
+}
+void setup()
+{
+    servo_6.attach(6, 500, 2500);
+}
+void loop()
+{
+    servo_6.write(90);
+    V_distance = 0.01723 * readUltrasonicDistance(7, 7);
+    if (V_distance <= 100)
+    {
+        servo_6.write(180);
+        delay(3000); // Wait for 3000 millisecond(s)
+        servo_6.write(90);
+    }
+    servo_6.write(90);
+}
+
 // Temperature Humidity
 #include <DHT.h>
 #include <ESP8266WiFi.h>
@@ -58,47 +92,4 @@ void loop()
         data_state = true;
     }
     delay(30000); // ThingSpeak will only accept updates every 15 seconds.
-}
-
-// Blynk App
-
-#define BLYNK_PRINT Serial
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-int sensorpin = A0;
-int sensorvalue = 0;
-int outputvalue = 0;
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = "lKT5GEt18dLeaUYw8os-lBfEeZ8ECM2d";
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "Komal";
-char pass[] = "komal1313";
-void setup()
-{
-    // Debug console
-    Serial.begin(9600);
-    Blynk.begin(auth, ssid, pass);
-}
-void loop()
-{
-    sensorvalue = analogRead(sensorpin);
-    outputvalue = map(sensorvalue, 0, 1023, 0, 100); // 100,0 pottupaaru
-    delay(1000);
-    if (outputvalue > 74)
-    {
-        Serial.println("water your plant");
-        Serial.print(outputvalue);
-        Blynk.notify("water your plant");
-        delay(1000);
-    }
-    else if (outputvalue < 45)
-    {
-        Serial.println("soil is wet enough to water");
-        Serial.print(outputvalue);
-        Blynk.notify("soil is wet enough to water");
-        delay(1000);
-    }
-    Blynk.run();
 }
